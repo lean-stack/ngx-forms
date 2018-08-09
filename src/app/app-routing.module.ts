0338aa1c045475192f9a302458f959bb172e7fc2
Micha Alt <micha@haus23.net>
2018-08-09 21:05:31 +0200

    Routing and pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TemplateDrivenComponent } from './pages/template-driven/template-driven.component';
+import { ModelDrivenComponent } from './pages/model-driven/model-driven.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'template-driven', component: TemplateDrivenComponent },
+  { path: 'model-driven', component: ModelDrivenComponent },
+  { path: '*', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateDrivenComponent } from './pages/template-driven/template-driven.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ModelDrivenComponent } from './pages/model-driven/model-driven.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenComponent,
+    HomeComponent,
+    ModelDrivenComponent
   ],
   imports: [
     BrowserModule,
