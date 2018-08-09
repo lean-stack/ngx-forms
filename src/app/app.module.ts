37ce388b2a2f20769d9b7643c92f993971e0c93b
Micha Alt <micha@haus23.net>
2018-08-09 21:21:28 +0200

    Navbar component created,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { TemplateDrivenComponent } from './pages/template-driven/template-driven.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ModelDrivenComponent } from './pages/model-driven/model-driven.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TemplateDrivenComponent,
     HomeComponent,
-    ModelDrivenComponent
+    ModelDrivenComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
