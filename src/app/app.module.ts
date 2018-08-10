5cad787bf61ae2c201cdc9feb0241568763a857f
Micha Alt <micha@haus23.net>
2018-08-10 09:02:59 +0200

    Model driven form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -22,6 +22,7 @@ import { PasswordMatchDirective } from './validation/password-match.directive';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/pages/model-driven/model-driven.component.ts
+++ src/app/pages/model-driven/model-driven.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { passwordMatchValidator } from '../../validation/password-match.directive';
 
 @Component({
   selector: 'app-model-driven',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelDrivenComponent implements OnInit {
 
-  constructor() { }
+  registerForm = new FormGroup({
+    acc: new FormControl('micha', [Validators.required, Validators.minLength(3)]),
+    pw1: new FormControl('', Validators.required),
+    pw2: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    zip: new FormControl('', Validators.pattern('[0-9]{5}'))
+  }, { validators: passwordMatchValidator });
+
+  get acc() { return this.registerForm.get('acc'); }
+
+  /*
+
+  registerForm = this.fb.group({
+    acc: ['micha', [Validators.required, Validators.minLength(3)]],
+    pw1: ['', Validators.required],
+    pw2: ['', [Validators.required]],
+    email: ['', Validators.required],
+    zip: ['', Validators.pattern('[0-9]{5}')],
+  }, { validators: passwordMatchValidator });
+
+  */
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    console.log(this.registerForm.value);
+  }
 }
